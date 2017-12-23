bf5563731454f70d5c035bfbe7a730ddcd5a22af
Demon000 <demonsingur@gmail.com>
2017-12-23 15:07:01 +0200

    sepolicy: common: add rules for candy vibrator hal
    Change-Id: I1e8aba0e0ce1a51cfcd54c734d58f10f0613f773

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,7 +1,13 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Lineage
+# inherit from Candy
 #
 
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/candy/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/candy/sepolicy/common/private
+
 BOARD_SEPOLICY_DIRS += \
-    device/candy/sepolicy/common
+   device/candy/sepolicy/common/vendor
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,6 +1,6 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Lineage
+# that inherit from Candy
 #
 
 BOARD_SEPOLICY_DIRS += \
